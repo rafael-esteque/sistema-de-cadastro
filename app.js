984d503ad3bc3ef7fc9db101cb4cd64a7b03a370
rafael-esteque <101527580+rafael-esteque@users.noreply.github.com>
2023-07-11 22:37:09 -0300

    Update app.js
    Criando um novo item no init do projeto

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectando ao banco de dados")
+console.log("Aplicação está se conectando ao banco de dados")
+console.log("Exibindo a lista de jogos")
